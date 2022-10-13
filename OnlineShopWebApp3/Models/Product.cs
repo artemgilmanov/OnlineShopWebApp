c4b1417337a623c7db661022522cd3cfddecd2ff
Artem Gilmanov <ag@rehub.team>
2022-10-13 23:05:05 +0200

    Add project files.

---
+++ OnlineShopWebApp3/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OnlineShopWebApp3.Models
+{
+    public class Product
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public decimal Cost { get; }
+        public string Description { get; }
+        public string ImagePath { get; }
+
+         public Product(Guid id, string name, decimal cost, string description, string imagePath)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Cost = cost;
+            Description = description;
+            ImagePath = imagePath;
+        }
+
+
+    }
+}
---
+++ OnlineShopWebApp3/ProductsRepository.cs
@@ -0,0 +1,23 @@
+﻿using OnlineShopWebApp3.Models;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace OnlineShopWebApp3
+{
+    public class ProductsRepository
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product(Guid.NewGuid(), "Product1", 100, "Description1", "/images/product1.png"),
+            new Product(Guid.NewGuid(), "Product2", 200, "Description2", "/images/product2.png"),
+            new Product(Guid.NewGuid(), "Product3", 300, "Description3", "/images/product3.png"),
+            new Product(Guid.NewGuid(), "Product4", 400, "Description4", "/images/product4.png"),
+            new Product(Guid.NewGuid(), "Product5", 500, "Description5", "/images/product5.png")
+        };
+        public  List<Product> GetAll()
+        {
+            return products;
+        }
+    }
+}
